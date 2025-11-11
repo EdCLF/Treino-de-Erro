96776c2fb86bd243284bf0b667e6822ad1f6d45d
EdCLF <dinho_canuto@yahoo.com.br>
2025-11-11 16:08:31 -0300

    Update mecanica.py
    começando

--- mecanica.py
+++ mecanica.py
@@ -1,15 +1,31 @@
 #Sistema de controle de mecanica
 
-veiculo = []
-
+contagem= {'nome':'joao','veiculo':'ford k','servico':'lataria'}
+chave =0
 
 while True:
     try:
         def registra_veiculo():
-
+            while True:
+                nome = input("digite o nome do proprietario: ")
+                veiculo = input("digite o veiculo: ")
+                servico = input("digite o servico: ")
+                contagem[f'{'nome'+str(chave+1)}']=[nome]
+                contagem[f'{'veiculo'+str(chave+1)}']=[veiculo]
+                contagem[f'{'servico' + str(chave + 1)}'] = [servico]
+                para = input('deseja registrar mais algum carro (s/n): ')
+                if para == "n":
+                    break
+                else:
+                    continue
+        def listar_veiculos():
+            print(contagem)
     except TypeError:
         print("Erro de tipo de dados")
     except ValueError:
         print("Erro de valor de dados")
     finally:
-        print()
+        print('ola')
+        registra_veiculo()
+        listar_veiculos()
+        break
